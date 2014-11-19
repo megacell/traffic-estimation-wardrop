69771cd2c5e7d80d7f5c7d4ee94d34e0a46871a0
Jerome <jerome.thai.87@gmail.com>
2014-11-18 21:51:10 -0800

    setup postgit and test

--- postgis_test.py
+++ postgis_test.py
@@ -5,17 +5,34 @@ Created on Nov 18, 2014
 '''
 
 import psycopg2
+import utm # https://pypi.python.org/pypi/utm
+
+
+def test_utm():
+    print utm.from_latlon(51.2, 7.5)
+
+def connect():
+    conn = psycopg2.connect(dbname='template_postgis', user='postgres')
+    cur = conn.cursor()
+    cur.execute("""SELECT srtext FROM spatial_ref_sys WHERE srid = 26910;""")
+    #cur.execute("""SELECT srtext FROM spatial_ref_sys;""")
+    rows = cur.fetchall()
+    for row in rows: print row
+
+
+def select():
+    conn = psycopg2.connect(dbname='ca-census', user='postgres')
+    cur = conn.cursor() # creates cursor that establishes connection
+    cur.execute("""SELECT * FROM ca_census_data;""")
+    rows = cur.fetchall()
+    for row in rows: print row
+
 
 def main():
-    print 'import sucessful'
-    #conn = psycopg2.connect(dbname='template_postgis', user='postgres',
-    #    host='localhost', password='jht2115')
-    conn = psycopg2.connect(dbname='template_postgis', user='postgres')
-    print 'connection successful'
-    cur = conn.cursor()
-    cur.execute("""SELECT srtext FROM spatial_ref_sys WHERE srid = 36910;""")
-    rows = cur.fetchall()
-    for row in rows: print "    ", row[1]
+    #select()
+    #create_table()
+    #connect()
+    test_utm()
 
 if __name__ == '__main__':
     main()
