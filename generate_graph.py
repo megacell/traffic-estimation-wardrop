7c6882f79267826e54f9328d71ee536489889f68
Jerome <jerome.thai.87@gmail.com>
2014-12-10 18:25:07 -0800

    Revert 2d2aacf..52c59b0
    This rolls back to commit 2d2aacfb17a81847fb5189c57cc8d51194a97705.

--- generate_graph.py
+++ generate_graph.py
@@ -3,14 +3,14 @@ Created on Apr 18, 2014
 
 @author: jeromethai
 '''
-import graph as g
+
+import Graph as g
 import numpy as np
 import scipy.io as sio
 from cvxopt import matrix
 from numpy.random import normal
 from util import distance_on_unit_sphere
-import draw_graph as d
-from Get_ODs_from_csv import Create_ODs_nodes_unique
+
 
 def small_example():
     
@@ -39,13 +39,15 @@ def small_example():
     return graph
 
 
-def los_angeles(parameters=None, delaytype='None', noise=0.0):
-    
-    data = sio.loadmat('los_angeles_data_2.mat')
+def los_angeles(parameters=None, delaytype='None', noise=0.0, path=None):
+    """Generate small map of L.A. with 122 links and 44 modes
+    """
+
+    if not path:
+        path = 'los_angeles_data_2.mat'
+    data = sio.loadmat(path)
         
     links = data['links']
-    #print 'links printing'
-    #print links
     ODs1, ODs2, ODs3, ODs4 = data['ODs1'], data['ODs2'], data['ODs3'], data['ODs4']
     if noise>0.0:
         ODs1 = [(o, d, normal(f, noise*f)) for o,d,f in ODs1]
@@ -55,10 +57,9 @@ def los_angeles(parameters=None, delaytype='None', noise=0.0):
         links = [(s, t, r, normal(d, noise*d), c) for s,t,r,d,c in links]
       
     nodes = data['nodes']
-    #print 'nodes printing'
-    #print nodes
     tmp = links
-    links=[]
+    links = []
+    
     if delaytype=='Polynomial':
         theta = parameters
         degree = len(theta)
@@ -72,76 +73,54 @@ def los_angeles(parameters=None, delaytype='None', noise=0.0):
             links.append((startnode, endnode, route, ffdelay, (ffdelay, slope, k1, k2)))
     if delaytype=='None':
         for startnode, endnode, route, ffdelay, slope in tmp: links.append((startnode, endnode, route, ffdelay, None))
-       
-    print links[0:2]
+            
     
     g1 = g.create_graph_from_list(nodes, links, delaytype, ODs1, 'Map of L.A.')
     g2 = g.create_graph_from_list(nodes, links, delaytype, ODs2, 'Map of L.A.')
     g3 = g.create_graph_from_list(nodes, links, delaytype, ODs3, 'Map of L.A.')
     g4 = g.create_graph_from_list(nodes, links, delaytype, ODs4, 'Map of L.A.')
-    #return g1, g2, g3, g4
-    return g1
-    
+    return g1, g2, g3, g4
+
 
 def los_angeles_2(parameters=None, delaytype='None'):
     """Generate larger map of L.A. with 664 links and 194 nodes
     """
-    nodes = np.genfromtxt('LA_medium_data/nodes_LA_toy.csv', delimiter = ',', skiprows = 1)
-    nodes = nodes[:,1:3]
-    links = np.genfromtxt('LA_medium_data/links_qgis_cap.csv', delimiter = ',', skiprows = 1)
+    data = sio.loadmat('los_angeles_data_3.mat')
+    nodes, links = data['nodes'], data['links']
     tmp = links
     links = []
-    speed_limit_freeway = 33.33 #unit: m/s
-    dict_cap2speed ={600:12.5, 1000:16.67, 2000:16.67, 4000:16.67, 5000:16.67, 1500:16.67, 3000:22.22, 6000:22.22, 9000:22.22, 4500:22.22, 7500:22.22, 10500:22.22}
+    a1 = 60.0*3960.0/75.0
+    a2 = 60.0*3960.0/50.0
     
     if delaytype=='None':
         for startnode, endnode, category in tmp:
-            arc = distance_on_unit_sphere(nodes[startnode-1][2], nodes[startnode-1][1], nodes[endnode-1][2], nodes[endnode-1][1])
-            if category == 1: ffdelay = arc/speed_limit_freeway
-            if category == 2: ffdelay = arc/16.67
-            if category !=0: links.append((startnode, endnode, 1, ffdelay, None))
+            arc = distance_on_unit_sphere(nodes[startnode-1][1], nodes[startnode-1][0], nodes[endnode-1][1], nodes[endnode-1][0])
+            if category == 1: ffdelay = a1*arc
+            if category == 2: ffdelay = a2*arc
+            links.append((startnode, endnode, 1, ffdelay, None))
             
     if delaytype=='Polynomial':
         theta = parameters
         degree = len(theta)
-        for startnode, endnode, category, cap in tmp:
+        for startnode, endnode, category in tmp:
             arc = distance_on_unit_sphere(nodes[startnode-1][1], nodes[startnode-1][0], nodes[endnode-1][1], nodes[endnode-1][0])
-            if category == 1: ffdelay, slope = arc/speed_limit_freeway, 1/cap
-            if category == 2: 
-                if dict_cap2speed.has_key(cap):           
-                    ffdelay, slope = arc/dict_cap2speed[cap], 1/cap
-                else : ffdelay, slope = arc/16.67, 1/cap
+            if category == 1: ffdelay, slope = a1*arc, 5.0
+            if category == 2: ffdelay, slope = a2*arc, 1.0
             coef = [ffdelay*a*b for a,b in zip(theta, np.power(slope, range(1,degree+1)))]
             links.append((startnode, endnode, 1, ffdelay, (ffdelay, slope, coef)))
-    
-    dest1 = 50
-    dest2 = 100
-    ODs=[]
-    
-    #ODs+=create_linear_ODs(34.044801, -117.831116, 33.955998, -118.309021, 10, 10, nodes, 6000.0)
-    #ODs+=create_linear_ODs(34.162493, -118.301468, 34.106226, -117.903214, 2, 20, nodes, 6000.0)    
-    #ODs+=create_linear_ODs(34.044801, -117.831116, 33.955998, -118.309021, 2, 30, nodes, 6000.0)
-    #ODs+=create_linear_ODs(34.162493, -118.301468, 34.106226, -117.903214, 2, 40, nodes, 6000.0)    
-    #ODs+=create_linear_ODs(34.044801, -117.831116, 33.955998, -118.309021, 2, 50, nodes, 6000.0)
-    #ODs+=create_linear_ODs(34.162493, -118.301468, 34.106226, -117.903214, 2, 60, nodes, 6000.0)    
-    #ODs+=create_linear_ODs(34.044801, -117.831116, 33.955998, -118.309021, 2, 70, nodes, 6000.0)
-    #ODs+=create_linear_ODs(34.162493, -118.301468, 34.106226, -117.903214, 2, 80, nodes, 6000.0)        
-        
-    #ODs = [[  57. ,   50.  ,   4. ],[  20. ,  162.   , 56.4]]
-    ODs = Create_ODs_nodes_unique(nodes)
-    ODs = ODs[1:16]
-    print ODs
-    return g.create_graph_from_list(nodes, links, delaytype, ODs, 'Larger map of L.A.')
 
-
+    #print nodes
+    #print links
+    return g.create_graph_from_list(nodes, links, delaytype, None, 'Larger map of L.A.')
 
 
 def main():
-    theta = matrix([0.0, 0.0, 0.0, 0.15])
-    #graph = los_angeles_2(theta, 'Polynomial', 1/15.0)[0]
-    graph = los_angeles_2(theta, 'Polynomial')
+    #graph = small_example()
+    #theta = matrix([0.0, 0.0, 0.0, 0.15])
+    #graph = los_angeles(theta, 'Polynomial', 1/15.0)[0]
     #graph.visualize(True, True, True, True, True)
-    d.draw(graph, nodes=False)
+    graph = los_angeles_2(None, 'None')
+    graph.visualize(True)
 
 if __name__ == '__main__':
     main()
--- ue_solver_test.py
+++ ue_solver_test.py
@@ -7,7 +7,7 @@ Created on Apr 21, 2014
 import numpy as np
 import ue_solver as ue
 import draw_graph as d
-from generate_graph import small_example, los_angeles, los_angeles_2
+from generate_graph import small_example, los_angeles
 from cvxopt import matrix, mul
 
 
@@ -22,8 +22,7 @@ def test1():
 def test2(delaytype):
     if delaytype == 'Polynomial': theta = matrix([0.0, 0.0, 0.0, 0.15, 0.0, 0.0])
     if delaytype == 'Hyperbolic': theta = (3.5, 3.0)
-    #g = los_angeles(theta, delaytype)[3]
-    g = los_angeles_2(theta, delaytype)
+    g = los_angeles(theta, delaytype)[3]
     n = g.numlinks
     l, x = ue.solver(g, update=True, full=True)
     d.draw_delays(g, x[:n])
